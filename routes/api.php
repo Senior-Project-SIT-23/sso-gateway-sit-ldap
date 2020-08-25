1e660ff6ee3df469d578da7ef65bb1e3b671b093
pepea23 <pepea.23@live.com>
2020-08-25 10:13:34 +0700

    :school_satchel: Update api

--- routes/api.php
+++ routes/api.php
@@ -16,17 +16,16 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::group(['middleware' => ['ByPassCORS']], function () {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: *');
-    header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
 
-    Route::post('/login', 'LDAPController@attemptLogin');
-    Route::get('/oauth/token', 'AuthController@getTokenByAuthCode');
-    Route::get('/applications/{app_id}', 'AuthController@getClient');
-    Route::group(['middleware' => ['checkAuth']], function () {
-        Route::get('/login/auth-code', 'AuthController@getAuthCodeByUserId');
-        Route::get('/logout', 'AuthController@logout');
-        Route::get('/me', 'AuthController@getUserByToken');
-    });
+Route::post('/login', 'LDAPController@attemptLogin');
+Route::get('/oauth/token', 'AuthController@getTokenByAuthCode');
+Route::get('/applications/{app_id}', 'AuthController@getClient');
+
+Route::group(['middleware' => ['checkAuth']], function () {
+    Route::get('/login/auth-code', 'AuthController@getAuthCodeByUserId');
+    Route::get('/logout', 'AuthController@logout');
+    Route::get('/me', 'AuthController@getUserByToken');
 });
