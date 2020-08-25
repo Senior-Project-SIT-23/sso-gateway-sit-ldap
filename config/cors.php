26f1cb9772cb9294826034576996071dc3b6e4c8
pepea23 <pepea.23@live.com>
2020-08-25 09:26:40 +0700

    :school_satchel: Update cor

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*, http://localhost:3000', 'http://std-sso-fe.sit.kmutt.ac.th'],
 
     'allowed_origins_patterns' => [],
 
